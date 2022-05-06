a161fa07c79014cbc502d6af9d261aae417a5ee8
Laurent Quenel <laurent@MacBook-Pro-de-Laurent-2.local>
2022-05-06 13:16:37 +0200

    prevent empty body

---
+++ rifts.md
@@ -0,0 +1,8 @@
+## RIFTS LIST
+
+# Login.mustache:
+- Missing CSRF token
+
+# server.js
+/login (post):
+- Missing data validation
--- server.js
+++ server.js
@@ -1,141 +1,183 @@
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const sqlite3 = require("sqlite3").verbose();
 
-const express = require('express')
-const mustacheExpress = require('mustache-express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const cookieSession = require('cookie-session')
-const flash = require('connect-flash')
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const sqlite3 = require('sqlite3').verbose()
+const { body, validationResult } = require("express-validator");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.engine('mustache', mustacheExpress())
-app.set('view engine', 'mustache')
-app.set('views', __dirname + '/views')
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
 
-app.use(cookieParser())
-app.use(cookieSession({
-  name: 'session',
-  keys: ['cats'],
-  maxAge: 24 * 60 * 60 * 1000
-}))
-app.use(flash())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(cookieParser());
+app.use(
+   cookieSession({
+      name: "session",
+      keys: ["cats"],
+      maxAge: 24 * 60 * 60 * 1000,
+   })
+);
+app.use(flash());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const users = [
-  { username: 'marty', password: 'i-love-hoverboards', email: 'marty.mcfly@hill-valley.com' },
-  { username: 'doc', password: 'einstein', email: 'mad-scientisty@hill-valley.com' }
-]
+   { username: "marty", password: "i-love-hoverboards", email: "marty.mcfly@hill-valley.com" },
+   { username: "doc", password: "einstein", email: "mad-scientisty@hill-valley.com" },
+];
 
-const guests = [{ name: 'Madonna'}, { name: 'Luke Skywalker' }]
+const guests = [{ name: "Madonna" }, { name: "Luke Skywalker" }];
 
-const db = new sqlite3.Database(':memory:')
+const db = new sqlite3.Database(":memory:");
 db.serialize(() => {
-  db.run('CREATE TABLE users (id integer primary key, username varchar(255), email varchar(255), password varchar(1024))')
-  users.forEach(({ username, email, password }) => {
-    db.run(`INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`)
-  })
+   db.run("CREATE TABLE users (id integer primary key, username varchar(255), email varchar(255), password varchar(1024))");
+   users.forEach(({ username, email, password }) => {
+      db.run(`INSERT INTO users (username, email, password) VALUES ('${username}', '${email}', '${password}')`);
+   });
 
-  db.run('CREATE TABLE guests (id integer primary key, name varchar(20))')
-  guests.forEach(({ name }) => {
-    db.run(`INSERT INTO guests (name) VALUES ('${name}')`)
-  })
-})
+   db.run("CREATE TABLE guests (id integer primary key, name varchar(20))");
+   guests.forEach(({ name }) => {
+      db.run(`INSERT INTO guests (name) VALUES ('${name}')`);
+   });
+});
 
-passport.use(new LocalStrategy(
-  (username, password, done) => {
-    console.log(`Looking for user ${username}`)
-    db.get(`SELECT id, username, email, password FROM users WHERE username = '${username}'`, (error, user) => {
-      console.log(`Found him: ${JSON.stringify(user)}`)
-      if (error) {
-        console.log('Error')
-        return done(error)
-      }
+passport.use(
+   new LocalStrategy((username, password, done) => {
+      console.log(`Looking for user ${username}`);
+      db.get(`SELECT id, username, email, password FROM users WHERE username = '${username}'`, (error, user) => {
+         console.log(`Found him: ${JSON.stringify(user)}`);
+         if (error) {
+            console.log("Error");
+            return done(error);
+         }
 
-      if (!user) {
-        console.log('User does not exist')
-        return done(null, false)
-      }
+         if (!user) {
+            console.log("User does not exist");
+            return done(null, false);
+         }
 
-      if (password !== user.password) {
-        console.log('Password is wrong')
-        return done(null, false)
-      }
+         if (password !== user.password) {
+            console.log("Password is wrong");
+            return done(null, false);
+         }
 
-      const { id, username, email } = user
-      console.log(`Authenticating user ${username}`)
-      return done(null, { id, username, email })
-    })
-  }
-))
+         const { id, username, email } = user;
+         console.log(`Authenticating user ${username}`);
+         return done(null, { id, username, email });
+      });
+   })
+);
 
 passport.serializeUser((user, done) => {
-  done(null, user)
-})
+   done(null, user);
+});
 
 passport.deserializeUser((user, done) => {
-  return done(null, user)
-})
+   return done(null, user);
+});
 
-const isConnected = req => req.session.passport && req.session.passport.user
+const isConnected = (req) => req.session.passport && req.session.passport.user;
 
 const redirectIfAnonymous = (req, res, next) => {
-  if (isConnected(req)) {
-    next()
-  } else {
-    res.redirect('/login')
-  }
-}
+   if (isConnected(req)) {
+      next();
+   } else {
+      res.redirect("/login");
+   }
+};
 
 const redirectIfConnect = (req, res, next) => {
-  if (isConnected(req)) {
-    res.redirect('/')
-  } else {
-    next()
-  }
-}
+   if (isConnected(req)) {
+      res.redirect("/");
+   } else {
+      next();
+   }
+};
 
-app.get('/', redirectIfAnonymous, (req, res) => {
-  const { user } = req.session.passport
-  const welcomeMessage = req.query.welcome ? req.query.welcome : 'Welcome to the party !'
+app.get("/", redirectIfAnonymous, (req, res) => {
+   const { user } = req.session.passport;
+   const welcomeMessage = req.query.welcome ? req.query.welcome : "Welcome to the party !";
 
-  db.all('SELECT name FROM guests', (err, rows) => {
-    res.render('guest-list.mustache', { user, welcomeMessage, guests: rows.map(({ name }) => name) })
-  })
-})
+   db.all("SELECT name FROM guests", (err, rows) => {
+      res.render("guest-list.mustache", {
+         user,
+         welcomeMessage,
+         guests: rows.map(({ name }) => name),
+      });
+   });
+});
 
-app.get('/logout', (req, res) => {
-  req.session = null
-  res.redirect('/login')
-})
+app.get("/logout", (req, res) => {
+   req.session = null;
+   res.redirect("/login");
+});
 
-app.get('/login', redirectIfConnect, (req, res) => {
-  console.log('/login')
-  res.render('login.mustache', { flash: req.flash('error') } )
-})
+app.get("/login", redirectIfConnect, (req, res) => {
+   console.log("/login");
+   res.render("login.mustache", { flash: req.flash("error") });
+});
 
-app.post('/login',
-  (req, res, next) => {
-    console.log(`Trying to authenticate with following credentials: ${req.body.username} ${req.body.password}`)
-    next()
-  },
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  })
-)
+/* Missing data validation  */
+app.post(
+   "/login",
+   body("username").isLength({ min: 4, max: 30 }).withMessage("firstname must be beetween 4 and 40 carac").not().isEmpty().withMessage("firstname is missing"),
+   body("password")
+      .not()
+      .isEmpty()
+      .withMessage("password is missing")
+      .isStrongPassword({
+         minLength: 8,
+         minLowercase: 1,
+         minUppercase: 1,
+         minNumbers: 1,
+         minSymbols: 1,
+         returnScore: false,
+         pointsPerUnique: 1,
+         pointsPerRepeat: 0.5,
+         pointsForContainingLower: 10,
+         pointsForContainingUpper: 10,
+         pointsForContainingNumber: 10,
+         pointsForContainingSymbol: 10,
+      })
+      .withMessage("password must be  valid"),
+   (req, res, next) => {
+      /* console.log(`Trying to authenticate with following credentials: ${req.body.username} ${req.body.password}`); */
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({ errors: errors.array() });
+      }
+      next();
+   },
+   passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+      failureFlash: true,
+   })
+);
 
-app.post('/invite', (req, res) => {
-  db.exec(`INSERT INTO guests (name) VALUES ('${req.body.guest}')`)
-  res.redirect('/')
-})
+/* Missing data validation  */
+app.post(
+   "/invite",
+   body("guest").isLength({ min: 4, max: 30 }).withMessage("Guest name must be beetween 4 and 40 carac").not().isEmpty().withMessage("Guest is missing"),
+   (req, res) => {
+      db.exec(`INSERT INTO guests (name) VALUES ('${req.body.guest}')`);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+         return res.status(400).json({ errors: errors.array() });
+      }
+      res.redirect("/");
+   }
+);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+   console.log(`Example app listening at http://localhost:${port}`);
+});
